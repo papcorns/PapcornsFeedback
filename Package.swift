30bd6fc1f48665da5d17df7a399bf05522af63ed
kadirAltintop <kadiraltintop61@gmail.com>
2023-03-30 03:21:14 +0300

    xcassets add module

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "PapcornsFeedback",
-            dependencies: []),
+            dependencies: [], resources: [.copy("feedbackAssets.xcassets")]),
         .testTarget(
             name: "PapcornsFeedbackTests",
             dependencies: ["PapcornsFeedback"]),
