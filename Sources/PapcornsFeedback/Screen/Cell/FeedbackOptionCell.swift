a829137e38ece96d3e494fb4bd786a2516efd7d0
kadirAltintop <kadiraltintop61@gmail.com>
2023-03-30 03:11:48 +0300

    Bundle.module fix

--- Sources/PapcornsFeedback/Screen/Cell/FeedbackOptionCell.swift
+++ Sources/PapcornsFeedback/Screen/Cell/FeedbackOptionCell.swift
@@ -13,18 +13,18 @@ class FeedbackOptionCell: UITableViewCell {
     @IBOutlet weak var lblTitle: UILabel!
     @IBOutlet weak var containerView: UIView!
     
-//    override var isSelected: Bool{
-//        get {
-//            super.isSelected
-//        }
-//        set{
-//            if newValue {
-//                imgSelection.image = UIImage(named: "feedback_selected_icon")!
-//            }else {
-//                imgSelection.image = UIImage(named: "feedback_unselected_icon")!
-//            }
-//        }
-//    }
+    override var isSelected: Bool{
+        get {
+            super.isSelected
+        }
+        set{
+            if newValue {
+                imgSelection.image = UIImage(named: "feedback_selected_icon")!
+            }else {
+                imgSelection.image = UIImage(named: "feedback_unselected_icon")!
+            }
+        }
+    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -35,10 +35,11 @@ class FeedbackOptionCell: UITableViewCell {
     }
     
     func prepareCell(with model: FeedbackConfig, row: Int) {
+        backgroundColor = .clear
         containerView.backgroundColor = model.cellBackgroundColor
         lblTitle.textColor = model.cellTextColor
         lblTitle.text = model.feedbackTypes[row].title
-        //containerView.layer.cornerRadius = CGFloat(model.cellCornerRadius ?? 10)
+        containerView.layer.cornerRadius = CGFloat(model.cellCornerRadius ?? 10)
         if let font = model.cellTextFont {
             lblTitle.font = font
         }
