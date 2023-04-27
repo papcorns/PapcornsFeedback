054018b84bd002e388d218ca90062abbf6ca5d45
kadirAltintop <kadiraltintop61@gmail.com>
2023-04-27 15:15:43 +0300

    test 3

--- Sources/PapcornsFeedback/Screen/Controller/PapcornsFeedbackViewController.swift
+++ Sources/PapcornsFeedback/Screen/Controller/PapcornsFeedbackViewController.swift
@@ -26,8 +26,8 @@ class PapcornsFeedbackViewController: UIViewController {
         setupUI()
     }
 
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
        
