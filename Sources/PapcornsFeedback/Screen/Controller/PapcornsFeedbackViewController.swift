7e2637621a1d818bdb005b4f55fab8be5808f991
kadirAltintop <kadiraltintop61@gmail.com>
2023-04-27 13:11:35 +0300

    resign

--- Sources/PapcornsFeedback/Screen/Controller/PapcornsFeedbackViewController.swift
+++ Sources/PapcornsFeedback/Screen/Controller/PapcornsFeedbackViewController.swift
@@ -22,6 +22,7 @@ class PapcornsFeedbackViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.resignFirstResponder()
         setupUI()
     }
 
